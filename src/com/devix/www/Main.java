369983a358e5a7da9b917a0e92c8f57f3e177670
Carlos Anguiano <c.joseanguiano@gmail.com>
2017-08-30 08:16:24 -0500

    Java program that uses String constructor

---
+++ src/com/devix/www/Main.java
@@ -0,0 +1,10 @@
+package com.devix.www;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte[] array = {97, 98, 99};
+        String result = new String(array);
+        System.out.println(result);
+    }
+}
